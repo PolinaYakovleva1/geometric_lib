dc6fa81ec02b6036ae627afa8287d7ba6326be8e
Polina Yakovleva <yakovlevap146@gmail.com>
2024-11-23 01:32:01 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,6 +1,7 @@
 def area(a, b, c):
     if a >= 0 and b >= 0 and c >= 0:
-        return (a + b + c) / 2
+        p = (a + b + c) / 2
+        return (p * (p - a) * (p - b) * (p - c))**0.5
     else:
         raise ValueError("Error. The size must be greater than zero.")
 
