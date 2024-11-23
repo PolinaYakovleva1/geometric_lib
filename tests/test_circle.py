3de1a3c3df9bdd65368d64f95e8f25c9e15d4043
Polina Yakovleva <yakovlevap146@gmail.com>
2024-11-23 16:39:52 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -10,8 +10,6 @@ class CircleTest(unittest.TestCase):
                          78.53981633974483)
         self.assertEqual(calculate.calc("circle", "area", [2]),
                          12.566370614359172)
-        self.assertEqual(calculate.calc("circle", "area", [1]),
-                         3.141592653589793)
 
     def test_area_negative(self):
         error_message = "Size should be > 0."
@@ -22,17 +20,12 @@ class CircleTest(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             calculate.calc("circle", "area", [-52])
         self.assertEqual(str(context.exception), error_message)
-        with self.assertRaises(ValueError) as context:
-            calculate.calc("circle", "area", [-9])
-        self.assertEqual(str(context.exception), error_message)
 
     """Perimeter"""
 
     def test_perimeter_positive(self):
         self.assertEqual(calculate.calc("circle", "perimeter", [1]),
                          6.283185307179586)
-        self.assertEqual(calculate.calc("circle", "perimeter", [3]),
-                         18.84955592153876)
         self.assertEqual(calculate.calc("circle", "perimeter", [52]),
                          326.7256359733385)
 
@@ -40,10 +33,6 @@ class CircleTest(unittest.TestCase):
         error_message = "Size should be > 0."
         with self.assertRaises(ValueError) as context:
             calculate.calc("circle", "perimeter", [-5])
-        self.assertEqual(str(context.exception), error_message)
-
-        with self.assertRaises(ValueError) as context:
-            calculate.calc("circle", "perimeter", [-2])
         self.assertEqual(str(context.exception), error_message)
 
         with self.assertRaises(ValueError) as context:
