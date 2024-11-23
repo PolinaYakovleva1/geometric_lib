9097af08ed55722a57264ce6ecba3c9531148504
Polina Yakovleva <yakovlevap146@gmail.com>
2024-11-23 04:28:11 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -15,12 +15,14 @@ sizes = {
 
 
 def calc(fig, func, size):
-    assert fig in figs, f"Error figure: {fig}. Valid value: {figs}."
-    assert func in funcs, f"Error function: {func}. Valid value: {funcs}."
+    assert fig in figs, f"Error figure: {fig}. Valid value {figs}."
+    assert func in funcs, "Error function: {func}. Valid value \
+    {funcs}."
     assert len(size) == sizes.get(f"{func}-{fig}", 1), \
         f"Error number of parameters for {func} of {fig}."
     result = eval(f'{fig}.{func}(*{size})')
     return result
+
 
 if __name__ == "__main__":
     func = ''
@@ -29,15 +31,13 @@ if __name__ == "__main__":
 
     while fig not in figs:
         fig = input(f"Enter the name of the shape, "
-                    f"the following options are available: {figs}:\n")
+                    "the following options are available {figs}:\n")
 
     while func not in funcs:
         func = input(f"Enter the name of the function, "
-                     f"the following options are available: {funcs}:\n")
+                     "the following options are available {funcs}:\n")
 
     while len(size) != sizes.get(f"{func}-{fig}", 1):
-        size = list(map(int, input("Input figure sizes separated by space, "
-                                   "1 for circle and square:\n").split(' ')))
+        size = list(map(int, input("Input figure sizes separated by space, " "1 for circle and square\n").split(' ')))
 
     print(calc(fig, func, size))
-    
