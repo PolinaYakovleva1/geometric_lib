0cbb65ce96ce932dbfc418167e8c105867b2c05d
Polina Yakovleva <yakovlevap146@gmail.com>
2024-11-23 03:33:59 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -27,14 +27,14 @@ if __name__ == "__main__":
     func = ''
     fig = ''
     size = list()
-    
+
     while fig not in figs:
         fig = input(f"Enter the name of the shape, the following options are available {figs}:\n")
-    
+
     while func not in funcs:
         func = input(f"Enter the name of the function, the following options are available {funcs}:\n")
-    
+
     while len(size) != sizes.get(f"{func}-{fig}", 1):
         size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	print(calc(fig, func, size))
+    
+    print(calc(fig, func, size))
